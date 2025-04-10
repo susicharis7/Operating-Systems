6ac7df9e547f520c533a5d3130aff1bf6b0800b7
susicharis7 <susicharis99@gmail.com>
2025-04-10 15:18:01 +0200

    "a"

--- again-first-project.c
+++ again-first-project.c
@@ -10,27 +10,33 @@
 #include <sched.h>
 #include <time.h>
 
+// MAX size for `user input` , `num of args in commands` , stack size for child processes - clone() 
 #define BUFFER_SIZE 1024
 #define MAX_ARGS 64
 #define STACK_SIZE 1024 * 1024
 
+// `defining` colors for shell (1.4)
 #define GREEN   "\033[32m"
 #define CYAN    "\033[36m"
 #define RESET   "\033[0m"
 
+
+// executed by child process, created with `clone()` 
 int child_function(void *arg) {
-    printf("Child (clone): Pozivam execle() za /usr/bin/env\n");
+    printf("Child (clone): Calling `execle()` for /usr/bin/env\n");
     char *envp[] = {"MY_VAR=HelloWorld", NULL};
     execle("/usr/bin/env", "env", NULL, envp);
-    perror("execle nije uspeo");
+    perror("Execle failed!");
     exit(1);
 }
 
+// main
 int main() {
-    char input[BUFFER_SIZE];
-    char *shellname = "sanin-and-haris-project-shell";
+    char input[BUFFER_SIZE]; // buffer for `user input`
+    char *shellname = "sanin-and-haris-project-shelly";
     srand(time(NULL));
 
+// infinite loop (main shell logic, get methods) - (1.1)
     while (1) {
         char *username = getlogin();
         char hostname[HOST_NAME_MAX];
@@ -39,14 +45,16 @@ int main() {
         getcwd(cwd, sizeof(cwd));
         char *home = getenv("HOME");
 
+        // printing `shell prompt` (with colors and formatting)
         if (home != NULL && strstr(cwd, home) == cwd) {
             printf("%s[%s]%s %s%s@%s:~%s$ %s", GREEN, shellname, RESET, CYAN, username, hostname, cwd + strlen(home), RESET);
         } else {
             printf("%s[%s]%s %s%s@%s:%s$ %s", GREEN, shellname, RESET, CYAN, username, hostname, cwd, RESET);
         }
-
+        // for forcing output display 
         fflush(stdout);
 
+        // reads `user input`
         if (fgets(input, sizeof(input), stdin) == NULL) {
             printf("\n");
             break;
@@ -56,7 +64,7 @@ int main() {
         if (strlen(input) == 0) continue;
         if (strcmp(input, "exit") == 0) break;
 
-        // Redirekcija
+        // redirection 
         char *redirect_pos = strstr(input, ">");
         int is_redirecting = (redirect_pos != NULL);
         char *outfile = NULL;
@@ -68,7 +76,7 @@ int main() {
             outfile = redirect_pos;
         }
 
-        // Parsiranje komande
+        // parsing input into `cmd arguments`
         char *args[MAX_ARGS];
         int i = 0;
         char *token = strtok(input, " ");
@@ -78,25 +86,28 @@ int main() {
         }
         args[i] = NULL;
 
+	// built-in command : `cd`
         if (strcmp(args[0], "cd") == 0) {
             if (args[1] == NULL) {
-                fprintf(stderr, "cd: moraš uneti putanju\n");
+                fprintf(stderr, "cd: You have to enter path!\n");
             } else {
                 if (chdir(args[1]) != 0) {
-                    perror("cd nije uspeo");
+                    perror("CD Failed...");
                 }
             }
             continue;
         }
 
+	// `forkbomb` with demo 
         if (strcmp(args[0], "forkbomb") == 0) {
             char o;
-            printf("⚠️ UPOZORENJE: Ovo je forkbomb demo. Nastaviti? (y/n): ");
+            printf("THIS IS `FORKBOMB` Demo : continue? (y/n): ");
             scanf(" %c", &o);
             if (o != 'y') continue;
             while (1) fork();
         }
 
+	// uses `clone()` for creating new process
         if (strcmp(args[0], "runclone") == 0) {
             void *stack = malloc(STACK_SIZE);
             if (!stack) {
@@ -105,26 +116,29 @@ int main() {
             }
             pid_t pid = clone(child_function, stack + STACK_SIZE, SIGCHLD, NULL);
             if (pid == -1) {
-                perror("clone nije uspeo");
+                perror("Clone failed!");
                 free(stack);
                 continue;
             }
-            waitpid(pid, NULL, 0);
+            waitpid(pid, NULL, 0); // waits for `child process` to finish
             free(stack);
             continue;
         }
 
+	// info - prints user and machine info
         if (strcmp(args[0], "info") == 0) {
-            printf("Korisnik: %s\n", username);
-            printf("Mašina: %s\n", hostname);
+            printf("USER: %s\n", username);
+            printf("MACHINE: %s\n", hostname);
             continue;
         }
 
+	// random - generates a random number 
         if (strcmp(args[0], "random") == 0) {
-            printf("Nasumičan broj: %d\n", rand() % 100);
+            printf("Random Number Generator: %d\n", rand() % 100);
             continue;
         }
 
+	// countwords - counts words using `wc` (in file)
         if (strcmp(args[0], "countwords") == 0 && args[1]) {
             char clean_filename[BUFFER_SIZE];
             strncpy(clean_filename, args[1], BUFFER_SIZE);
@@ -137,14 +151,15 @@ int main() {
                 if (is_redirecting && outfile != NULL) {
                     int fd = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
                     if (fd < 0) {
-                        perror("Ne mogu da otvorim fajl");
+                        perror("Cannot open file!");
                         exit(1);
                     }
+		    // redirecting output to file
                     dup2(fd, STDOUT_FILENO);
                     close(fd);
                 }
                 execvp("wc", new_args);
-                perror("wc nije uspeo");
+                perror("`wc` failed!");
                 exit(1);
             } else {
                 wait(NULL);
@@ -152,24 +167,25 @@ int main() {
             }
         }
 
+	// executes other `external` commands using `fork()` & `execvp()`
         pid_t pid = fork();
         if (pid == 0) {
             if (is_redirecting && outfile != NULL) {
                 int fd = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
                 if (fd < 0) {
-                    perror("Nisam mogao da otvorim fajl");
+                    perror("Could not open the file!");
                     exit(1);
                 }
                 dup2(fd, STDOUT_FILENO);
                 close(fd);
             }
             execvp(args[0], args);
-            perror("Greška pri izvršavanju komande");
+            perror("Error while executing..");
             exit(1);
         } else if (pid > 0) {
             wait(NULL);
         } else {
-            perror("fork greška");
+            perror("Fork error!");
         }
     }
 
